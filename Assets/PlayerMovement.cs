aa6cc5ac7404cffc802ecb049f088f5a54aeab80
uknable <uknable@hotmail.com>
2020-10-21 17:28:50 +1100

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 6f;
+	public float gravity = 9.81f;
+	public CharacterController controller;
+	public Transform groundCheck;
+	public float groundDistance = 0.3f;
+	public LayerMask groundMask;
+
+	Vector3 velocity;
+	bool isGrounded;
+
+
+	void Update () {
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+		if (isGrounded && velocity.y < 0) {
+			velocity.y = -2f;
+		}
+
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 move = transform.right * x + transform.forward * z;
+
+		controller.Move(move * moveSpeed * Time.deltaTime);
+
+		velocity.y -= gravity * Time.deltaTime;
+
+		controller.Move(velocity * Time.deltaTime);
+	}
+}
