bb9ededb004e972baaa43150e472a479934f3766
uknable <uknable@hotmail.com>
2020-10-28 17:19:12 +1100

    doors windows

---
+++ Assets/DoorOpen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+	public Transform customPivot;
+
+	public float openSpeed;
+
+	Animator animator;
+	bool playerNear;
+
+	void Update()
+	{
+		
+
+		// transform.RotateAround(customPivot.position, Vector3.up, openSpeed * Time.deltaTime);
+	}
+}
