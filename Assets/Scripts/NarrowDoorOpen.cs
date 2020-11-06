46b427ff49b9c8b07edd5f882ad36012a7325a70
uknable <uknable@hotmail.com>
2020-11-07 08:17:45 +1100

    door opens

---
+++ Assets/Scripts/NarrowDoorOpen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrowDoorOpen : MonoBehaviour {
+
+	public float openSpeed;
+
+	Animator animator;
+	bool isOpen = false;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.tag == "Player" && Input.GetMouseButtonDown(0)) {
+			isOpen = !isOpen;
+			animator.SetBool("NarrowDoorOpen", isOpen);
+		}
+	}
+}
