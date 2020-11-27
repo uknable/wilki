2b4d109683376680c8f5d4f03e418a8899f85852
uknable <uknable@hotmail.com>
2020-11-27 13:11:06 +1100

    logic finished i think

---
+++ Assets/Scripts/HomeCorridorTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeCorridorTrigger : MonoBehaviour
+{
+    GameManager gmScript;
+    Animator doorHomeAnim;
+
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameObject doorHome;
+    [SerializeField] GameObject endBlockade;
+
+    void Start()
+    {
+        doorHomeAnim = doorHome.GetComponent<Animator>();
+        gmScript = gameManager.GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            gmScript.hasCard = false;
+        }
+
+        if (doorHomeAnim.GetBool("NarrowDoorOpen") == true) {
+            doorHomeAnim.SetBool("NarrowDoorOpen", false);
+        }
+
+        endBlockade.SetActive(true);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ReturnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject startingPosition;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            // CharacterController cc = other.GetComponent<CharacterController>();
+
+            // cc.enabled = false;
+            other.transform.position = startingPosition.transform.position;
+            // cc.enabled = true;
+        }
+    }
+}
